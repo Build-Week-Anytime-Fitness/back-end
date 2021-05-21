16b3b351179dc6f8946acd6c79e114ee616bbd8e
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2021-05-21 19:01:41 -0400

    Made changes to the configuration in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,11 @@ require('dotenv').config({ path: './.env' });
 
 const pg = require('pg');
 
+/*
 if (process.env.DATABASE_URL) {
   pg.defaults.ssl = { rejectUnauthorized: false };
 }
+*/
 
 const sharedConfig = {
   client: 'pg',
